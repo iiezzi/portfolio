c0e37b477c413e170fbd45c9a41477448e8716be
iiezzi <iezzi.2011@hotmail.com>
2022-04-25 23:59:44 -0300

    Modificacion target

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="container mt-5">
         <div class="row">
             <div class="col-12 col-sm-3">
-                <a href="https://github.com" title="GitHub">
+                <a href="https://github.com" title="GitHub" target="_blank">
                     <i class="fa-brands fa-github mx-2"></i></a>
 
                 <a href="https://www.linkedin.com/in/iñaki-iezzi-201275236/" title="Linkedin" target="_blank">
